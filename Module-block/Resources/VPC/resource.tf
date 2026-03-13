6ff2c8e39b4a62093c0b83ab723185c0175338a6
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-14 02:20:18 +0530

    Update resource.tf

--- Module-block/Resources/VPC/resource.tf
+++ Module-block/Resources/VPC/resource.tf
@@ -1,11 +1,9 @@
-
-
+#VPC
 resource "aws_vpc" "webserver_vpc" {
   cidr_block           = var.vpccidr
 }
 
-
-# 2. Create a Public Subnet
+#Subnet
 resource "aws_subnet" "webserver_subnet" {
   vpc_id                  = aws_vpc.webserver_vpc.id
   cidr_block              = var.subnetcidr

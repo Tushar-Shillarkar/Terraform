6d1e864041bffd1f086b791cef25012e2da7d62c
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-20 14:16:23 +0530

    Update main.tf

--- vartf/main.tf
+++ vartf/main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  profile = "configs"
+  region  = "ap-south-1"
+
+}
+resource "aws_instance" "webserver" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.sg]
+
+
+}
 

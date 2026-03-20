56a1a3348790bf6e37b9cfdc16130a1276844d8f
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-20 14:19:34 +0530

    Create var.tf

---
+++ vartf/var.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+  default = "ami-0f559c3642608c138"
+
+}
+
+variable "instance_type" {
+  default = "c7i-flex.large"
+
+}
+
+variable "key_name" {
+  default = "mum-key"
+
+}
+variable "sg" {
+  default = "sg-0663f01f023943ec3"
+}

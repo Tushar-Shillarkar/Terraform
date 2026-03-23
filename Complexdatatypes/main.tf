57b3734dfee8feef063b32a2f53ca599bb5e738e
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-23 14:41:40 +0530

    hf

--- Complexdatatypes/main.tf
+++ Complexdatatypes/main.tf
@@ -1,2 +1,11 @@
-provider "" "" {
+provider "aws" {
+profile = "configs"
+region = "ap-south-1"
+}
+
+resource "aws_instance" "webserver" {
+    amiid = var.amiid
+    instance_type = var.instance_type
+    key_name = var.key
+    vpc_security_groups_ids = ["sg-0663f01f023943ec3"]
 }
---
+++ Complexdatatypes/var.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  default     = "t3.micro"
+}
+
+variable "key" {
+    default = "mum-key"
+}
+
+variable " amiid" {
+    default = "ami-0f559c3642608c138"
+
+}

2359f0e67aecd84ea4330da22dc913c198f852d9
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-23 15:50:35 +0530

    fdfv

--- Complexdatatypes/main.tf
+++ Complexdatatypes/main.tf
@@ -5,7 +5,9 @@ region = "ap-south-1"
 
 resource "aws_instance" "webserver" {
     amiid = var.amiid
-    instance_type = var.instance_type
-    key_name = var.key
+    instance_type = var.this_list[0]
+    key_name = var.this_any.wskeymaps.kp1
     vpc_security_groups_ids = ["sg-0663f01f023943ec3"]
+    cout
 }
+

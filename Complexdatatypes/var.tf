12f20b5fb2433611e6361e8b3deff231aba01090
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-23 14:55:17 +0530

    fd

--- Complexdatatypes/var.tf
+++ Complexdatatypes/var.tf
@@ -1,12 +1,28 @@
-variable "instance_type" {
-  default     = "t3.micro"
+variable "this_list" {
+  default     = ["t3.micro"]
 }
 
-variable "key" {
-    default = "mum-key"
+variable "this_map" {
+    default = {
+        amiws = "ami-0f559c3642608c138"
+    }   keynamews = "mum-key"
 }
 
-variable " amiid" {
-    default = "ami-0f559c3642608c138"
 
+variable "this_any" {
+    default = {
+        wscount =2
+        wsinsatncetype = ["t3.micro" "t3.small" "c7i-flex.large"]
+        amiws = {
+            ami1ws = "ami-05d2d839d4f73aafb",
+            ami2ws = "ami-0f559c3642608c138"
+
+        }
+        wscountlist = [1 , 2 , 3]
+      wskeymaps = {
+        kp1 = "ohiokey"
+        kp2 = "vpc"
+      }
+
+    }
 }

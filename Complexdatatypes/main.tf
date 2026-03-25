a8c449219e5c731e3003df6c69526aeb24414296
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-25 20:21:08 +0530

    fd

--- Complexdatatypes/main.tf
+++ Complexdatatypes/main.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "webserver" {
-    amiid = var.amiid
+    ami = var.this_map.amiws
     instance_type = var.this_list[0]
     key_name = var.this_any.wskeymaps.kp1
     vpc_security_group_ids = ["sg-0663f01f023943ec3"]

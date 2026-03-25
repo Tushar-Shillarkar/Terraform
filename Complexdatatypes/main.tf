7b2ed9220ac758ae739c1fe326a77a55e46eaa92
Tushar Shillarkar <162004717+Tushar-Shillarkar@users.noreply.github.com>
2026-03-25 19:30:49 +0530

    ds

--- Complexdatatypes/main.tf
+++ Complexdatatypes/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "webserver" {
     amiid = var.amiid
     instance_type = var.this_list[0]
     key_name = var.this_any.wskeymaps.kp1
-    vpc_security_groups_ids = ["sg-0663f01f023943ec3"]
+    vpc_security_group_ids = ["sg-0663f01f023943ec3"]
   
 }
 
